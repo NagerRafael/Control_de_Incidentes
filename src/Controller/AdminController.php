d3b0dd215ef83de378edc5af19771e7163660371
Rafael Naranjo <rafael.naranjo@sasf.net>
2023-04-05 02:32:41 -0500

    pdf guía añadido

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/admin'), IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/', name: 'app_admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
